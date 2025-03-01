20dee0dd97b0059cba70751902be18612fd43406
mks1313 <mg.marinov@gmx.es>
2025-03-01 12:00:27 +0100

    getenv modified + main test

--- libft/src/str/ft_strchr.c
+++ libft/src/str/ft_strchr.c
@@ -6,7 +6,7 @@
 /*   By: mmarinov <mmarinov@student.42barcelon      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/06/25 18:08:31 by mmarinov          #+#    #+#             */
-/*   Updated: 2025/02/26 15:01:09 by mmarinov         ###   ########.fr       */
+/*   Updated: 2025/03/01 11:42:38 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,22 +24,22 @@ char	*ft_strchr(const char *s, int c)
 		return ((char *)s);
 	return (NULL);
 }
-
+/*
 #include <stdio.h>
 
 int	main(void)
 {
-char	*str = "Hello, World";
-int		f = 'o';
-char	*result = ft_strchr(str, f);
+	char	*str = "Hello, World";
+	int		f = 'o';
+	char	*result = ft_strchr(str, f);
 
-if (result != NULL)
-{
-printf("Caracer encontrado %s\n", result);
-}
-else
-{
-printf("Caracter no encontrado\n");
-}
-return (0);
-}
+	if (result != NULL)
+	{
+		printf("Caracer encontrado %s\n", result);
+	}
+	else
+	{
+		printf("Caracter no encontrado\n");
+	}
+	return (0);
+}*/
--- libft/src/utils/ft_getenv.c
+++ libft/src/utils/ft_getenv.c
@@ -6,7 +6,7 @@
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/27 14:43:35 by mmarinov          #+#    #+#             */
-/*   Updated: 2025/02/27 14:45:04 by mmarinov         ###   ########.fr       */
+/*   Updated: 2025/03/01 11:51:32 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,16 +17,16 @@ char	*ft_getenv(const char *name, char **envp) {
 	int		i;
 	size_t	len;
 
-	len = strlen(name);
-	i = 0;
 	// Si no se pasa un nombre de variable de entorno, retornar NULL
 	if (!name)
 		return NULL;
+	len = ft_strlen(name);
+	i = 0;
 	// Recorrer el arreglo de entorno (envp) manualmente
 	while (envp[i]) {
 		// Cada variable de entorno tiene formato "NOMBRE=VALOR"
 		// Comparar si "NOMBRE" coincide con el nombre proporcionado
-		if (strncmp(envp[i], name, len) == 0 && envp[i][len] == '=') {
+		if (ft_strncmp(envp[i], name, len) == 0 && envp[i][len] == '=') {
 			// Retornar el valor después del signo '='
 			return envp[i] + len + 1;
 		}
@@ -35,3 +35,11 @@ char	*ft_getenv(const char *name, char **envp) {
 	// Si no se encontró la variable, retornar NULL
 	return NULL;
 }
+/*
+int	main(int argc, char *argv[], char *envp[])
+{
+	ft_getenv("PATH", envp);
+	(void)argc;
+	(void)argv;
+}
+*/
