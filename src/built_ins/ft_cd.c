51940db117493d3e4bb941d13d00c5873d901b99
mks1313 <mg.marinov@gmx.es>
2025-03-23 17:40:10 +0100

    fixing norminette in ft_cd.c

--- src/built_ins/ft_cd.c
+++ src/built_ins/ft_cd.c
@@ -6,27 +6,31 @@
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/03 22:29:55 by mmarinov          #+#    #+#             */
-/*   Updated: 2025/03/10 18:26:36 by mmarinov         ###   ########.fr       */
+/*   Updated: 2025/03/23 17:38:59 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "minishell.h"
 
+// Si no hay argumento, cambiar al directorio home
+// Ahora funciona con t_env
+// Si hay un argumento, intentar cambiar al directorio indicado
+// Si no puede cambiar de directorio
 void	ft_cd(t_tkn *tokens, t_shell *shell)
 {
-	if (!tokens || !tokens->next)  // Si no hay argumento, cambiar al directorio home
+	char	*home;
+
+	if (!tokens || !tokens->next)
 	{
-		char *home = ft_getenv("HOME", shell->env);  // Ahora funciona con t_env
+		home = ft_getenv("HOME", shell->env);
 		if (home)
 			chdir(home);
 		else
 			ft_putstr_fd("minishell: cd: HOME not set\n", 2);
 	}
-	else  // Si hay un argumento, intentar cambiar al directorio indicado
+	else
 	{
-		if (chdir(tokens->next->value) == -1)  // Si no puede cambiar de directorio
-		{
+		if (chdir(tokens->next->value) == -1)
 			perror("minishell: cd");
-		}
 	}
 }
