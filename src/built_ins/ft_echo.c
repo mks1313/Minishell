0e28be66bb073d38ee481236ac321c429f4d36a4
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-03-29 13:48:17 +0100

    fix builtin echo, now -n is working

--- src/built_ins/ft_echo.c
+++ src/built_ins/ft_echo.c
@@ -6,7 +6,7 @@
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/02/20 16:14:06 by mmarinov          #+#    #+#             */
-/*   Updated: 2025/03/23 17:45:15 by mmarinov         ###   ########.fr       */
+/*   Updated: 2025/03/29 13:43:09 by meghribe         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,6 +18,38 @@
  * Separar los tokens por espacios
  * Salto de línea al final (a menos que sea -n)
  */
+
+/*
+ * Saltamos el primer token, ya que es el comando 'echo'
+ * Verificamos si hay opcion -n
+ * Imprimir argumentos, si hay mas tokens separlos por un espacio
+ * Salto de linea solo si no se encontro -n
+ */
+void	ft_echo(t_tkn *tokens)
+{
+	int	option_n;
+
+	if (!tokens)
+		return ;;
+	option_n = 0;
+	tokens = tokens->next;
+	if (tokens && ft_strcmp(tokens->value, "-n") == 0)
+	{
+		option_n = 1;
+		tokens = tokens->next;
+	}
+	while (tokens)
+	{
+		if (tokens->type == TOKEN_WORD)
+			ft_putstr_fd(tokens->value, 1);
+		if (tokens->next)
+			write(1, " ", 1);
+		tokens = tokens->next;
+	}
+	if (!option_n)
+		write(1, "\n", 1);
+}
+/*
 void	ft_echo(t_tkn *tokens)
 {
 	if (tokens->next && ft_strcmp(tokens->next->value, "-n") == 0)
@@ -31,4 +63,4 @@ void	ft_echo(t_tkn *tokens)
 		tokens = tokens->next;
 	}
 	write(1, "\n", 1);
-}
+}*/
