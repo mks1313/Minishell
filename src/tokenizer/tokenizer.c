98c348fcac63eca67314036c8d5ef1f79692c989
mks1313 <mg.marinov@gmx.es>
2025-04-27 18:29:17 +0200

    solved quotes

--- src/tokenizer/tokenizer.c
+++ src/tokenizer/tokenizer.c
@@ -6,7 +6,7 @@
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/03/10 17:04:41 by mmarinov          #+#    #+#             */
-/*   Updated: 2025/04/27 13:18:07 by mmarinov         ###   ########.fr       */
+/*   Updated: 2025/04/27 18:28:53 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,49 +14,51 @@
 
 char	*read_token_segment(char **str, bool *s_quote, bool *db_quote)
 {
-	char	*start = *str;
-	char	*result = NULL;
-	char	*temp;
+	char	*buffer;
 	char	quote;
 
+	buffer = ft_calloc(1, 1);
+	if (!buffer)
+		return (NULL);
 	*s_quote = false;
 	*db_quote = false;
-	if (**str == '\'' || **str == '"')
-	{
-		quote = *(*str)++;
-		if (quote == '\'') *s_quote = true;
-		if (quote == '"') *db_quote = true;
 
-		while (**str && **str != quote)
-		{
-			if (**str == '\\' && quote == '"')
-				(*str)++;
-			(*str)++;
-		}
-		if (**str == quote)
-			(*str)++;
-	}
-	else
+	while (**str && !ft_strchr(" \t\n|<>", **str))
 	{
-		while (**str && !ft_strchr(" \t\n|<>\"\'", **str))
+		if (**str == '\'' || **str == '"')
 		{
-			if (**str == '\\' && *(*str + 1)) (*str)++;
+			quote = *(*str)++;
+			if (quote == '\'')
+				*s_quote = true;
+			else if (quote == '"')
+				*db_quote = true;
+			while (**str && **str != quote)
+			{
+				buffer = ft_strjoin_free(buffer, ft_substr(*str, 0, 1));
+				(*str)++;
+			}
+			if (**str == quote)
+				(*str)++;
+		}
+		else
+		{
+			buffer = ft_strjoin_free(buffer, ft_substr(*str, 0, 1));
 			(*str)++;
 		}
 	}
-	temp = ft_substr(start, 0, *str - start);
-	if (!temp)
-		return (NULL);
-	result = temp;
-	return (result);
+	return (buffer);
 }
 
 static int	process_token(t_tkn **token, char **str)
 {
-	char	*accum = ft_strdup("");
+	char	*accum;
 	char	*seg;
-	bool	sq, dq;
+	bool	sq;
+	bool	dq;
+	char	*tmp;
+	t_tkn	*new;
 
+	accum = ft_strdup("");
 	skip_delimiters(str);
 	if (**str == '\0')
 		return (0);
@@ -68,17 +70,27 @@ static int	process_token(t_tkn **token, char **str)
 			free(accum);
 			return (0);
 		}
-		char *tmp = ft_strjoin(accum, seg);
+		tmp = ft_strjoin(accum, seg);
 		free(accum);
 		free(seg);
 		accum = tmp;
 	}
-	t_tkn *new = create_token(accum, TOK_WORD);
+	new = create_token(accum, TOK_WORD);
 	new->s_quote = sq;
 	new->db_quote = dq;
 	add_token_to_list(token, new, accum, accum + ft_strlen(accum));
 	free(accum);
 	return (1);
+}
+
+static void debug(t_tkn *head)
+{
+	t_tkn	*curr = head;
+		while (curr)
+		{
+			ft_printf("Token: [%s], Type: %d\n", curr->value, curr->type);
+			curr = curr->next;
+		}
 }
 
 t_tkn	*tokenize_input(char *line)
@@ -103,5 +115,6 @@ t_tkn	*tokenize_input(char *line)
 		}
 	}
 	printf("== Tokenization complete ==\n");
+	debug(token[HEAD]);
 	return (token[HEAD]);
 }
