48eb187971385c0082c7b6b984642852c68e916f
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-05-03 18:04:59 +0200

    removed unused functions and created string.c to save safe_strdup function"

---
+++ src/utils/string.c
@@ -0,0 +1,28 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   string.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: meghribe <meghribe@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/05/03 18:00:36 by meghribe          #+#    #+#             */
+/*   Updated: 2025/05/03 18:02:11 by meghribe         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "minishell.h"
+
+/**
+ * @brief Duplicates the string with null verification
+ * 
+ * This function checks the string before try to duplicate. To avoid segfaults
+ * 
+ * @param s The string to duplicate
+ * @return The copied string or empty string if its NULL
+ */
+char	*safe_strdup(const char *s)
+{
+	if (!s)
+		return (ft_strdup(""));
+	return (ft_strdup(s));
+}
