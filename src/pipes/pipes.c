29243de6b6f9a0d2dbde5da55c01cdc6807a5386
mks1313 <mg.marinov@gmx.es>
2025-05-07 18:40:24 +0200

    nomri

--- src/pipes/pipes.c
+++ src/pipes/pipes.c
@@ -6,7 +6,7 @@
 /*   By: mmarinov <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/04/21 13:11:48 by mmarinov          #+#    #+#             */
-/*   Updated: 2025/05/07 12:54:56 by mmarinov         ###   ########.fr       */
+/*   Updated: 2025/05/07 18:28:19 by mmarinov         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,13 +55,48 @@ int	execute_single_command(t_cmd *cmd, t_env *env)
 // Esperar a que todos los hijos terminen
 // Esperar a todos los procesos hijos
 // Guardar el estado de salida final
+static void	child_process(t_cmd *curr, int prev_fd, int *pipefd, t_shell *shell)
+{
+	if (prev_fd != -1)
+		dup2(prev_fd, STDIN_FILENO);
+	if (curr->next)
+		dup2(pipefd[1], STDOUT_FILENO);
+	if (prev_fd != -1)
+		close(prev_fd);
+	if (curr->next)
+	{
+		close(pipefd[0]);
+		close(pipefd[1]);
+	}
+	handle_redirections(curr);
+	if (is_builtin_command(curr->cmd))
+		handle_builtin_commands(curr, shell, NULL);
+	else
+		execute_single_command(curr, shell->env);
+	exit(EXIT_FAILURE);
+}
+
+static void	wait_for_children(t_cmd *cmd_list, t_shell *shell)
+{
+	int		status;
+	t_cmd	*curr;
+
+	curr = cmd_list;
+	while (curr)
+	{
+		waitpid(-1, &status, 0);
+		if (WIFEXITED(status))
+			shell->exit_status = WEXITSTATUS(status);
+		curr = curr->next;
+	}
+}
+
 void	execute_piped_commands(t_cmd *cmd_list, t_shell *shell)
 {
 	int		pipefd[2];
 	int		prev_fd;
 	pid_t	pid;
 	t_cmd	*curr;
-	int		status;
 
 	curr = cmd_list;
 	prev_fd = -1;
@@ -71,25 +106,7 @@ void	execute_piped_commands(t_cmd *cmd_list, t_shell *shell)
 			pipe(pipefd);
 		pid = fork();
 		if (pid == 0)
-		{
-			if (prev_fd != -1)
-				dup2(prev_fd, STDIN_FILENO);
-			if (curr->next)
-				dup2(pipefd[1], STDOUT_FILENO);
-			if (prev_fd != -1)
-				close(prev_fd);
-			if (curr->next)
-			{
-				close(pipefd[0]);
-				close(pipefd[1]);
-			}
-			handle_redirections(curr);
-			if (is_builtin_command(curr->cmd))
-				handle_builtin_commands(curr, shell, NULL);
-			else
-				execute_single_command(curr, shell->env);
-			exit(EXIT_FAILURE);
-		}
+			child_process(curr, prev_fd, pipefd, shell);
 		if (prev_fd != -1)
 			close(prev_fd);
 		if (curr->next)
@@ -99,12 +116,5 @@ void	execute_piped_commands(t_cmd *cmd_list, t_shell *shell)
 		}
 		curr = curr->next;
 	}
-	curr = cmd_list;
-	while (curr)
-	{
-		waitpid(-1, &status, 0);
-		if (WIFEXITED(status))
-			shell->exit_status = WEXITSTATUS(status);
-		curr = curr->next;
-	}
+	wait_for_children(cmd_list, shell);
 }
