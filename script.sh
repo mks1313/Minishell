50746a6363aff573ba0e0888800ecb70f0f20902
mgrl39 <170405410+mgrl39@users.noreply.github.com>
2025-05-10 16:34:36 +0200

    add noninteractive version of minishell

---
+++ script.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+TEST_CASES_FILE="test_cases.txt"
+
+GREEN='\033[0;32m'
+RED='\033[0;31m'
+NC='\033[0m'
+
+BASH="/bin/bash"
+MINISHELL="./minishell"
+
+i=0
+
+while IFS= read -r cmd; do
+	i=$((i + 1))
+	bash_output=$(echo "$cmd" | $BASH 2>&1)
+	minishell_raw_output=$(echo "$cmd" | $MINISHELL 2>&1)
+
+	minishell_output=$(echo "$minishell_raw_output" | sed -e '/^minishell\$/d' -e '/^exit$/d' | sed ':a;N;$!ba;s/\n/ /g' | sed 's/ *$//g')
+
+	if [ "$bash_output" == "$minishell_output" ]; then
+		echo -e "${GREEN}Test #$i: OK${NC} -> $cmd"
+	else
+		echo -e "${RED}Test #$i: KO${NC} -> $cmd"
+		echo -e "${RED}Bash Output:${NC} \"$bash_output\""
+		echo -e "${RED}Minishell Output:${NC} \"$minishell_output\""
+	fi
+done < "$TEST_CASES_FILE"
